14ab9853e642fab7a35faacb350652ce40d0bc97
moonxsugar <nomikiparginou@gmail.com>
2021-08-21 13:17:30 +0300

    final

---
+++ Controllers/TracksController.cs
@@ -0,0 +1,205 @@
+﻿using Sitecore.FakeDb;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class TracksController : Controller
+    {
+        private ChinookEntities db = new ChinookEntities();
+
+        public object Configuration { get; private set; }
+
+        // GET: Tracks
+        public ActionResult Index()
+        {
+            var tracks = db.Tracks.Include(t => t.Album).Include(t => t.Genre).Include(t => t.MediaType);
+            return View(tracks.ToList());
+        }
+
+        // GET: Tracks/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Track track = db.Tracks.Find(id);
+            if (track == null)
+            {
+                return HttpNotFound();
+            }
+            return View(track);
+        }
+
+        // GET: Tracks/Create
+        public ActionResult Create()
+        {
+            ViewBag.AlbumId = new SelectList(db.Albums, "AlbumId", "Title");
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name");
+            ViewBag.MediaTypeId = new SelectList(db.MediaTypes, "MediaTypeId", "Name");
+            return View();
+        }
+
+        // POST: Tracks/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TrackId,Name,AlbumId,MediaTypeId,GenreId,Composer,Milliseconds,Bytes,UnitPrice")] Track track)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tracks.Add(track);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AlbumId = new SelectList(db.Albums, "AlbumId", "Title", track.AlbumId);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", track.GenreId);
+            ViewBag.MediaTypeId = new SelectList(db.MediaTypes, "MediaTypeId", "Name", track.MediaTypeId);
+            return View(track);
+        }
+
+        // GET: Tracks/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Track track = db.Tracks.Find(id);
+            if (track == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AlbumId = new SelectList(db.Albums, "AlbumId", "Title", track.AlbumId);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", track.GenreId);
+            ViewBag.MediaTypeId = new SelectList(db.MediaTypes, "MediaTypeId", "Name", track.MediaTypeId);
+            return View(track);
+        }
+
+        // POST: Tracks/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TrackId,Name,AlbumId,MediaTypeId,GenreId,Composer,Milliseconds,Bytes,UnitPrice")] Track track)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(track).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AlbumId = new SelectList(db.Albums, "AlbumId", "Title", track.AlbumId);
+            ViewBag.GenreId = new SelectList(db.Genres, "GenreId", "Name", track.GenreId);
+            ViewBag.MediaTypeId = new SelectList(db.MediaTypes, "MediaTypeId", "Name", track.MediaTypeId);
+            return View(track);
+        }
+
+        // GET: Tracks/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Track track = db.Tracks.Find(id);
+            if (track == null)
+            {
+                return HttpNotFound();
+            }
+            return View(track);
+        }
+
+        // POST: Tracks/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Track track = db.Tracks.Find(id);
+            db.Tracks.Remove(track);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        //methods for Report II
+
+        public ActionResult InputReportII()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InputReportII(string beginDate, string endDate)
+        {
+            using (var context = new ChinookEntities())
+            {
+
+                if (beginDate.Equals("") || endDate.Equals(""))
+                    return RedirectToAction("InputReportII");
+
+                SqlParameter[] Parameters = {
+                    new SqlParameter("@BeginDate", beginDate),
+                    new SqlParameter("@EndDate", endDate)
+                };
+                var repII = context.Database.SqlQuery<ReportII_Result>("[dbo].[ReportII] @Begindate, @EndDate", Parameters).ToList();
+                TempData["repII"] = repII;
+                return RedirectToAction("printReportII");
+            }
+        }
+
+
+        public ActionResult printReportII()
+        {
+            return View(TempData["repII"]);
+        }
+
+        //methods for ReportVI
+        public ActionResult InputReportVI()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InputReportVI(int Year)
+        {
+            using (var context = new ChinookEntities())
+            {
+                if(Year.Equals(""))
+                    return RedirectToAction("InputReportVI");
+
+                SqlParameter[] Parameters = {
+                    new SqlParameter("@Year", Year)
+                };
+                var repVI = context.Database.SqlQuery<ReportVI_Result>("[dbo].[ReportVI] @Year", Parameters).ToList();
+                TempData["repVI"] = repVI;
+                return RedirectToAction("printReportVI");
+            }
+        }
+
+        public ActionResult printReportVI()
+        {
+            return View(TempData["repVI"]);
+        }
+
+    }
+}
